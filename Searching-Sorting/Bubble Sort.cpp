95230093f3f986c1eed21385c8a839e0ad3b28b1
Mayank Tomar <81947542+MayankTomar21@users.noreply.github.com>
2022-07-15 20:08:32 +0530

    Update Bubble Sort.cpp

--- Searching-Sorting/Bubble Sort.cpp
+++ Searching-Sorting/Bubble Sort.cpp
@@ -1,3 +1,5 @@
+/*************TIME COMPLEXITY == BEST-O(n), Worst/Average - O(n^2) ****************************//
+    
 #include <iostream>
 using namespace std;
 
@@ -28,4 +30,69 @@ int main() {
     bubbleSort(data,size);
     cout << "Sorted Array In Ascending order: ";
     printArray(data,size);
+}
+
+//************************************************************OPTIMIZED SOLUTION*****************************************************************************
+
+/* this code, all comparisons are made even if array is already Sorted = this increases execution time
+Optimized Bubble Sort:  To Solve this, extra variable swapped is used - in which if there is no swapping needed, the value of swapped variable will be false
+
+Optimized Code: 
+
+// Optimized bubble sort in C++ */
+
+#include <iostream>
+using namespace std;
+
+// perform bubble sort
+void bubbleSort(int array[], int size) {
+    
+  // loop to access each array element
+  for (int step = 0; step < (size-1); ++step) {
+      
+    // check if swapping occurs
+    int swapped = 0;
+    
+    // loop to compare two elements
+    for (int i = 0; i < (size-step-1); ++i) {
+        
+      // compare two array elements
+      // change > to < to sort in descending order
+      if (array[i] > array[i + 1]) {
+
+        // swapping occurs if elements
+        // are not in intended order 
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        
+        swapped = 1;
+      }
+    }
+
+    // no swapping means the array is already sorted
+    // so no need of further comparison
+    if (swapped == 0)
+      break;
+  }
+}
+
+// print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    cout << "  " << array[i];
+  }
+  cout << "\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  
+  // find the array's length
+  int size = sizeof(data) / sizeof(data[0]);
+  
+  bubbleSort(data, size);
+  
+  cout << "Sorted Array in Ascending Order:\n";
+  printArray(data, size);
 }
